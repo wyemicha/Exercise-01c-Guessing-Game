01bb82cc9b7cbf94dc990874085605c2878b4e85
Wyeth <wyemicha@iu.edu>
2021-01-31 15:01:13 -0500

    Created Guessing game in main and added number of guesses in main2

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 #!/usr/bin/env python3
 import sys
 assert sys.version_info >= (3,9), "This script requires at least Python 3.9"
+number = 5 
+guess = input("Guess a number from 1 to 10: ") 
+guess = int(guess) 
+if guess == number: 
+    print("Great job! You got it!") 
+else: 
+    print("Sorry, better luck next time.") 
+    print("The number was " + str(number))
--- main2.py
+++ main2.py
@@ -1,3 +1,20 @@
 #!/usr/bin/env python3
 import sys
+import random
 assert sys.version_info >= (3,9), "This script requires at least Python 3.9"
+number = random.randrange(1,3)
+totguess = 3
+while(totguess != 0):
+    guess = input("Guess a number from 1 to 10. You have "  + str(totguess) + " guesses left: ") 
+    guess = int(guess)
+    
+    if guess == number: 
+        print("Great job! You got it!") 
+        break
+    if  guess != number:
+        totguess -= 1
+        if guess != number and totguess == 0:
+            print("Looks like you've run out of guesses!")
+            break
+        print("Sorry my guy, try again!")
+   
